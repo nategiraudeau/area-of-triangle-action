23c80882ab03cc61f76a427d2b64536354b1c9ab
NateGiraudeau <50968964+NateGiraudeau@users.noreply.github.com>
2020-03-25 19:04:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const core = require('@actions/core');
 const github = require('@actions/github');
+const core = require('@actions/core');
 
 const width = core.getInput('width');
 const height = core.getInput('height');
