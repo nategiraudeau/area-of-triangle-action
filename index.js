ab47d1fc98d31449abaf185cb475462c9e9c55ab
NateGiraudeau <50968964+NateGiraudeau@users.noreply.github.com>
2020-03-25 19:02:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-
-  const width = core.getInput('width');
-  const height = core.getInput('height');
-  let area = (width*height)/2;
-  console.log(`the area of the triangle is ${area}.`);
-  core.setOutput("area", area);
-  
-} catch (error) {
-  core.setFailed(error.message);
-}
+const width = core.getInput('width');
+const height = core.getInput('height');
+let area = (width*height)/2;
+console.log(`the area of the triangle is ${area}.`);
+core.setOutput("area", area);
